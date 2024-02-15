9d1400a99cb7e5ad59a316c7ecad8eb872c36b9c
Amarsh Anand <amarsh.anand@coupa.com>
2024-02-15 09:18:37 -0800

    sdcdsc

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,13 +2,6 @@ import { createTheme } from "@mui/material/styles";
 import horizontal from "./horizontal";
 import vertical from "./vertical";
 
-console.dir(horizontal);
-console.dir(vertical);
-console.dir({
-  ...horizontal,
-  ...vertical,
-});
-
 const clarityTheme = createTheme({
   ...horizontal,
   ...vertical,
